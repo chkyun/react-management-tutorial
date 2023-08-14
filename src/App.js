d9a6b7500bfd2d446c32dad150280c365faca06c
chkyun <chkyun@naver.com>
2023-08-14 14:14:11 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import Customer from './component/Customer'
+import * as React from 'react';
+import Customer from './component/Customer';
 import './App.css';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import { withStyles } from '@mui/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: '10px',
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [{
   'id': 1,
@@ -28,26 +47,27 @@ const customers = [{
 
 ]
 
-function App() {
+function App(props) {
+  const { classes } = props;
   return (
-    <div>
-      {
-      customers.map(customers => {
-        return (          
-          <Customer 
-            key={customers.id}
-            id={customers.id}
-            image={customers.image}
-            name={customers.name}
-            birthday={customers.birthday}
-            gender={customers.gender}
-            job={customers.job}
-          />
-        );
-      })
-    }
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        { customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); }) }
+        </TableBody>
+    </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
