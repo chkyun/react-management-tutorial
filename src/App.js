13090fffe21e0f7f7cb356df27f1ff7a311f6234
chkyun <chkyun@naver.com>
2023-08-14 11:24:31 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
+import Customer from './component/Customer'
 import './App.css';
+
+const customers = [{
+  'id': 1,
+  'image': "https://placeimg.com/64/64/any",
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': "https://placeimg.com/64/64/2",
+  'name': '이순신',
+  'birthday': '951222',
+  'gender': '남자',
+  'job': '디자이너'
+},
+{
+  'id': 3,
+  'image': "https://placeimg.com/64/64/3",
+  'name': '강감찬',
+  'birthday': '971222',
+  'gender': '남자',
+  'job': '개발자'
+}
+
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+      customers.map(customers => {
+        return (          
+          <Customer 
+            key={customers.id}
+            id={customers.id}
+            image={customers.image}
+            name={customers.name}
+            birthday={customers.birthday}
+            gender={customers.gender}
+            job={customers.job}
+          />
+        );
+      })
+    }
     </div>
   );
 }
